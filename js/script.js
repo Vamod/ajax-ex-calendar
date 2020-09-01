8b1c47775bd0cba7578d12ba08f9eb0ec3b55db0
Valerio <valerio.modesti@gmail.com>
2020-09-01 07:59:36 +0200

    aggiunte festività

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
 $(document).ready(function(){
+    var attributo = $('h1').attr('data-this-date', '2018-02-01');
+    console.log(attributo);
+
+    $('#next').click(function(){
+
+    });
+
+
     //data di partenza è il 28-01-01
     // creo oggetto moment con questa data
     var dataCorrente = moment('2018-01-01');
 
-    var month = dataCorrente.format('MMMM');
-    var year = dataCorrente.format('YYYY');
+        insertDays(dataCorrente);
+        insertHolidays(dataCorrente);
 
-    $('h1.month').html(month + '' + year);
+});
 
-    var daysMonth = dataCorrente.daysInMonth();
+function insertHolidays(data){
+    $.ajax(
+        {
+            url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+            method: 'GET',
+            data: {
+                year: data.year(),
+                month: data.month()
+            },
+            success: function(risposta){
+                console.log(risposta.response);
+                for(var i = 0; i < risposta.response.length; i++){
+                    var listItem = $('li[data-complete-date='+ risposta.response[i].date +']');
+                    console.log(listItem);
+                    listItem.append('-'+ risposta.response[i].name);
+                    listItem.addClass('holiday');
+                }
+            },
+            error: function(){
+                alert('errore')
+            }
+        }
+    );
+}
+
+function insertDays(data){
+
+    var month = data.format('MMMM');
+    var year = data.format('YYYY');
+
+    $('h1.month').html(month + ' ' + year);
+
+    var daysMonth = data.daysInMonth();
 
     for (var i = 1; i <= daysMonth; i++) {
         var source = $('#day-template').html();
@@ -17,16 +57,14 @@ $(document).ready(function(){
         var context = {
             day: addZero(i),
             month: month,
-            completeDate: year + '-' + dataCorrente.format('MM') + '-' + addZero(i)
-
+            completeDate: year + '-' + data.format('MM') + '-' + addZero(i)
         }
 
         var html = template(context);
 
         $('.month-list').append(html);
     }
-
-});
+}
 
 function addZero(n){
     if (n < 10) {
